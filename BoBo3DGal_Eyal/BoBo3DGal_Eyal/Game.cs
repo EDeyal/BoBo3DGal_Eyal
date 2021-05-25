72d5ad02ef5ebee0245fff1d1f35260759a1ffd6
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-05-26 00:59:50 +0300

    ClassDiagram

---
+++ BoBo3DGal_Eyal/BoBo3DGal_Eyal/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoBo3DGal_Eyal
+{
+    public class Game
+    {
+        public Game()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool LoadScene(Scene scene)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Scene ActiveScene()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NewGame(Scene scene)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float DeltaTime()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
