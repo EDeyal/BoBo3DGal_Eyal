ec7597ea5a6b951261760b6383954a4159602146
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-05-27 01:46:21 +0300

    Rigidbody Updates

---
+++ BoBo3DGal_Eyal/BoBo3DGal_Eyal/BoxCollider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoBo3DGal_Eyal
+{
+    public class BoxCollider : Component
+    {
+        readonly Vector3 _size;
+    }
+}
---
+++ BoBo3DGal_Eyal/BoBo3DGal_Eyal/Ridigbody.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoBo3DGal_Eyal
+{
+    public class Ridigbooty : Component
+    {
+        #region Fields
+        private float _velocity;
+        private bool _useGravity;
+        private float _gravityScale;
+        #endregion
+
+        #region Properties
+        public float Velocity { get => _velocity; set => _velocity = value; }
+        public bool UseGravity { get => _useGravity; set => _useGravity = value; }
+        public float GravityScale { get => _gravityScale; set => _gravityScale = value; }
+        #endregion
+
+        public Vector3 AddForce()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ApplyConstantForce(Vector3 vector3, float amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
