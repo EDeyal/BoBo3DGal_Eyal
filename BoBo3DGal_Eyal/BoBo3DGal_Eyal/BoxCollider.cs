ee612eed9e50ca39249f782d44a4596a76305f4a
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-05-27 02:01:23 +0300

    Box Collider

--- BoBo3DGal_Eyal/BoBo3DGal_Eyal/BoxCollider.cs
+++ BoBo3DGal_Eyal/BoBo3DGal_Eyal/BoxCollider.cs
@@ -7,6 +7,23 @@ namespace BoBo3DGal_Eyal
 {
     public class BoxCollider : Component
     {
-        readonly Vector3 _size;
+        #region Fields
+        readonly Vector3 _size = new Vector3(1,1,1);
+        #endregion
+        #region Properties
+        Vector3 GetSize => _size;
+        #endregion
+        public void OnCollision()
+        {
+
+        }
+        public void OnCollisionStart()
+        {
+
+        }
+        public void OnCollisionEnd()
+        {
+
+        }
     }
 }
