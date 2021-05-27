df4ddf2bf63084c4def976891e7ea4b3985051bc
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-05-27 03:11:58 +0300

    Game Update

--- BoBo3DGal_Eyal/BoBo3DGal_Eyal/Game.cs
+++ BoBo3DGal_Eyal/BoBo3DGal_Eyal/Game.cs
@@ -7,34 +7,51 @@ namespace BoBo3DGal_Eyal
 {
     public class Game
     {
-        public Game()
+        #region Fields
+        float _deltaTime;
+        Scene _currentScene;
+        List<Scene> _sceneList;
+        #endregion
+
+        #region Properties
+        float DeltaTime { get => _deltaTime; set => _deltaTime = value; }
+        Scene CurrentScene { get => _currentScene; set => _currentScene = value; }
+        List<Scene> SceneList { get => _sceneList; set => _sceneList = value; }
+        #endregion
+
+        public Game(Scene scene)
         {
-            throw new System.NotImplementedException();
+            SceneList.Add(scene);
         }
 
         public bool Run()
         {
-            throw new System.NotImplementedException();
+            if (ActiveScene() == null)
+            {
+                Console.WriteLine("Error in Run");
+                return false;
+            }
+            LoadScene(ActiveScene());
+            return true;
         }
-
         public bool LoadScene(Scene scene)
         {
-            throw new System.NotImplementedException();
+            if(scene == null)
+            {
+                Console.WriteLine("Error in LoadScene");
+                return false;
+            }
+            scene.Start();
+            return true;
         }
-
         public Scene ActiveScene()
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void NewGame(Scene scene)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public float DeltaTime()
-        {
-            throw new System.NotImplementedException();
+            if(CurrentScene == null)
+            {
+                Console.WriteLine("Error in Active Scene");
+                return null;
+            }
+            return CurrentScene;
         }
     }
 }
